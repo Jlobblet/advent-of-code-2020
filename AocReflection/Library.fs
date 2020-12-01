ecfe855a4218b33c7dcc886909f994b09318dc55
Jlobblet <jlobblet@gmx.com>
2020-12-01 18:21:07 +0000

    Format code properly

--- AocReflection/Library.fs
+++ AocReflection/Library.fs
@@ -7,4 +7,18 @@ open System.Reflection
 type SolutionAttribute (Label: string) =
     inherit Attribute ()
 
-module Reflection=let getSolutions()=Directory.GetFiles(FileInfo(Assembly.GetExecutingAssembly().Location).Directory.FullName, "Day*.dll")|>Array.map(fun a->Assembly.LoadFile a)|>Array.collect (fun ass->ass.GetTypes())|>Array.collect(fun t->t.GetMethods())|>Array.filter(fun m->(m.CustomAttributes|>Seq.map(fun a->a.AttributeType)|>Seq.contains typeof<SolutionAttribute>))|>Array.map(fun m->(m.CustomAttributes|>Seq.find(fun a->a.AttributeType=typeof<SolutionAttribute>)|>(fun a->a.ConstructorArguments|>Seq.head|>string)).Replace("\"",""),m)
+module Reflection =
+
+    let getSolutions () =
+        let assemblies = FileInfo(Assembly.GetExecutingAssembly().Location).Directory.FullName
+        Directory.GetFiles(assemblies, "Day*.dll")
+        |> Array.map (fun a -> Assembly.LoadFile a)
+        |> Array.collect (fun ass -> ass.GetTypes())
+        |> Array.collect (fun t -> t.GetMethods())
+        |> Array.filter (fun m ->
+            (m.CustomAttributes
+             |> Seq.map (fun a -> a.AttributeType)
+             |> Seq.contains typeof<SolutionAttribute>))
+        |> Array.map (fun m -> (m.CustomAttributes
+                                 |> Seq.find (fun a -> a.AttributeType = typeof<SolutionAttribute>)
+                                 |> (fun a -> a.ConstructorArguments |> Seq.head |> string)).Replace("\"", ""), m)
