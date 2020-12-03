f2a2864f9acb174295ce8bf758d9b44be795cfa0
Jlobblet <jlobblet@gmx.com>
2020-12-03 05:44:43 +0000

    Add Seq.filteri documentation

--- Day3/SeqExtenion.fs
+++ Day3/SeqExtenion.fs
@@ -1,5 +1,7 @@
 [<RequireQualifiedAccess>]
 module Seq
 
+/// Returns a new collection containing only the elements of the collection for which the given predicate returns "true".
+/// The integer passed to the function indicates the index (from 0) of the element being tested.
 let filteri pred =
     Seq.indexed >> Seq.filter pred >> Seq.map snd
