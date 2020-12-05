a492632a8eb56ffd5f055d85dbda59e7ea840474
Jlobblet <jlobblet@gmx.com>
2020-12-05 01:33:50 +0000

    Move generators and solutions to separate files

---
+++ Day3/Generator.fs
@@ -0,0 +1,26 @@
+module Day3.Generator
+
+open AocReflection
+open Day3
+
+[<Generator("3")>]
+let generate (rnd: System.Random) ``width of block`` ``height Of block`` ``mean tree density`` =
+    let cellToChar cell =
+        match cell with
+        | Open -> '.'
+        | Tree -> '#'
+
+    let rowToString row =
+        row |> Array.map cellToChar |> System.String
+
+    let generateRow width =
+        Array.init width (fun _ ->
+            match rnd.NextDouble() <= ``mean tree density`` with
+            | true -> Tree
+            | false -> Open)
+
+    let map =
+        Array.init ``height Of block`` (fun _ -> generateRow ``width of block``)
+    map.[0].[0] <- Open
+
+    map |> Array.map rowToString |> String.concat "\n"
--- Day3/Solution.fs
+++ Day3/Solution.fs
@@ -43,25 +43,3 @@ let SolutionB () =
       Solve (Right 1) (Down 2) ]
     |> List.reduce (*)
     |> string
-
-[<Generator("3")>]
-let generate (rnd: System.Random) ``width of block`` ``height Of block`` ``mean tree density`` =
-    let cellToChar cell =
-        match cell with
-        | Open -> '.'
-        | Tree -> '#'
-
-    let rowToString row =
-        row |> Array.map cellToChar |> System.String
-
-    let generateRow width =
-        Array.init width (fun _ ->
-            match rnd.NextDouble() <= ``mean tree density`` with
-            | true -> Tree
-            | false -> Open)
-
-    let map =
-        Array.init ``height Of block`` (fun _ -> generateRow ``width of block``)
-    map.[0].[0] <- Open
-
-    map |> Array.map rowToString |> String.concat "\n"
