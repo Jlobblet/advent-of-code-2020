9efca8847a443ea53b9b0613e7bc303479791ff9
Jlobblet <jlobblet@gmx.com>
2020-12-06 05:24:45 +0000

    Day 6

---
+++ Day6/Library.fs
@@ -0,0 +1,33 @@
+﻿module Day6.Day6
+
+open System.IO
+open AocReflection
+
+let getInput location =
+    // normalise EOL character
+    File.ReadAllLines(location)
+    |> String.concat "\n"
+    |> (fun s -> s.Split("\n\n"))
+
+
+[<Solution("6A")>]
+let SolutionA input =
+    getInput input
+    |> Array.map
+        ((fun s -> s.Replace("\n", "").ToCharArray())
+         >> Set.ofArray
+         >> Set.count)
+    |> Array.sum
+    |> string
+
+[<Solution("6B")>]
+let SolutionB input =
+    getInput input
+    |> Array.map
+        ((fun s ->
+            s.Split("\n")
+            |> Array.map (fun s' -> s'.ToCharArray() |> Set.ofArray)
+            |> Array.reduce Set.intersect)
+         >> Set.count)
+    |> Array.sum
+    |> string
