f98da229e2a5774cb9e3df8ba025a5098f0a594b
Jlobblet <jlobblet@gmx.com>
2020-12-10 20:04:48 +0000

    Faster 10B solution

--- src/lib/days/Day10/Solution.fs
+++ src/lib/days/Day10/Solution.fs
@@ -6,7 +6,7 @@ open System.Numerics
 open AocReflection
 open Timer.Timer
 
-type deltas = { ``1 jolt``: int; ``2 jolt``: int; ``3 jolt``: int }
+type deltas = { ``1 jolt``: bigint; ``2 jolt``: bigint; ``3 jolt``: bigint }
     with
         static member (+) (x, y) =
             { ``1 jolt`` = x.``1 jolt`` + y.``1 jolt``
@@ -54,24 +54,24 @@ let splitAtEvery v l =
 
 let (|GetInput|) =
     File.ReadAllLines
-    >> Array.map int
+    >> Array.map (int >> bigint)
 
 [<Solution("10A")>]
 let SolutionA (timer: Timer) (GetInput input) =
     timer.Lap "Reading input"
     input
     // Add element for 0 (socket) and max + 3 (device)
-    |> Array.append [| 0; Array.max input + 3 |]
+    |> Array.append [| 0I; Array.max input + 3I |]
     |> Array.sort
     |!> timer.Lap "Sorting array"
     |> Array.pairwise
     |> Array.fold (fun (delta) (elt1, elt2) ->
         match elt2 - elt1 with
-        | 3 -> delta + deltas.make(0, 0, 1)
-        | 2 -> delta + deltas.make(0, 1, 0)
-        | 1 -> delta + deltas.make(1, 0, 0)
+        | a when a = 3I -> delta + deltas.make(0I, 0I, 1I)
+        | a when a = 2I -> delta + deltas.make(0I, 1I, 0I)
+        | a when a = 1I -> delta + deltas.make(1I, 0I, 0I)
         | _ -> delta)
-        (deltas.make(0, 0, 0))
+        (deltas.make(0I, 0I, 0I))
     |!> timer.Lap "Folding"
     |> (fun d -> d.``1 jolt`` * d.``3 jolt``)
     |> string
@@ -84,7 +84,7 @@ let combinations list =
         | (_ :: []) :: q -> inner q (sum + 1I)
         | (a :: b :: l) :: q ->
             match a + b with
-            | gt when gt > 3 -> inner ((b :: l) :: q) sum
+            | gt when gt > 3I -> inner ((b :: l) :: q) sum
             | _ -> inner ((a + b :: l) :: (b :: l) :: q) sum
         
     inner [list] 0I
@@ -94,17 +94,42 @@ let SolutionB (timer: Timer) (GetInput input) =
     timer.Lap "Reading input"
     input
     |> Array.sort
-    |> Array.append [|0|]
+    |> Array.append [|0I|]
     |!> timer.Lap "Sorting"
     |> List.ofArray
     |> List.pairwise
     |> List.map (fun (a, b) -> b - a)
     |!> timer.Lap "Calculating differences"
-    |> splitAtEvery 3
+    |> splitAtEvery 3I
     |!> timer.Lap "Splitting list"
     |> List.map combinations
     |> List.reduce (*)
-//
-//    |> calculateDeltas
     |!> timer.Lap "Calculating number of combinations"
+    |> string
+
+[<Solution("10BF")>]
+let SolutionBF (timer: Timer) (GetInput input) =
+    timer.Lap "Reading input"
+    
+    let sorted =
+        input
+        |> Array.append [|0I|]
+        |> Array.sortDescending
+        |!> timer.Lap "Sorting"
+        
+    let map =
+        [| 0I .. Array.head sorted + 3I |]
+        |> Array.map (fun i -> i, 0I)
+        |> Map.ofArray
+        |> Map.change (Array.max sorted) (fun _ -> Some 1I)
+        |!> timer.Lap "Constructing map"
+        
+    sorted
+    |> Array.tail
+    |> Array.fold (fun acc elt ->
+        acc
+        |> Map.change elt (fun _ -> Some (acc.[elt + 1I] + acc.[elt + 2I] + acc.[elt + 3I])))
+        map
+    |!> timer.Lap "Folding"
+    |> Map.find 0I
     |> string
